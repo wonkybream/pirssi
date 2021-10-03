544abaf9bdd175ff04bc5e7e6e885e9aa1e1b9ec
wonkybream <sampo.l@protonmail.com>
2021-10-03 11:13:04 +0300

    Implement example code

--- example.py
+++ example.py
@@ -1,6 +1,9 @@
+import argparse
 import logging
+from time import sleep
 
 from pirssi.connection import Connection
+from pirssi.consumer import Consumer
 from pirssi.publisher import Publisher
 
 logging.basicConfig(
@@ -10,10 +13,50 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser(
+    description="Example for running Pirssi subscriber or consumer."
+)
+parser.add_argument("action", choices=["publisher", "consumer"])
+arguments = parser.parse_args()
+
+
+def run_example_publisher():
     publisher = Publisher(Connection())
     publisher.connect()
 
-    publisher.publish("I am alive")
+    messages_to_publish = [
+        "I command you to do something",
+        "I would like to query some information",
+        "Something weird happened and you should know about it"
+    ]
+
+    for message in messages_to_publish:
+        publisher.publish(message)
+        sleep(1)
 
     publisher.close()
+
+
+def example_handler(message):
+    print(f"I just handled a message: {message}")
+
+
+def run_example_consumer():
+    consumer = Consumer(Connection())
+    consumer.connect()
+
+    consumer.add_handler(example_handler)
+
+    try:
+        while True:
+            consumer.read_messages()
+            sleep(1)
+    except KeyboardInterrupt:
+        consumer.close()
+
+
+if __name__ == '__main__':
+    if arguments.action == "publisher":
+        run_example_publisher()
+    elif arguments.action == "consumer":
+        run_example_consumer()
